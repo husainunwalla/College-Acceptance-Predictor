e0631ab5020b6b00ffadac80ed5228758c0ff805
husainunwalla <mr.husainunwalla@gmail.com>
2022-03-26 18:25:46 +0530

    ui changes added login

--- Application/main.py
+++ Application/main.py
@@ -1,21 +1,76 @@
 
 # importing the necessary dependencies
-from flask import Flask, render_template, request,jsonify
+#Husain - Added new import for addional functionality
+#from flask import Flask, render_template, request,jsonify
+from flask import (
+    Flask,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+    jsonify
+)
 from flask_cors import CORS,cross_origin
 import pickle
 import pandas as pd
 
+#Husain - class to store user type data
+class User:
+    def __init__(self, id, username, password):
+        self.id = id
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
+users = []
+users.append(User(id=1, username='Husain', password='password'))
+users.append(User(id=2, username='Jainam', password='password'))
+users.append(User(id=3, username='Isha', password='password'))
+
 app = Flask(__name__) # initializing a flask app
+app.secret_key = 'somesecretkeythatonlyishouldknow'
+
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return render_template("form.html")
+
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
 
 @app.route('/',methods=['GET'])  # route to display the home page
 @cross_origin()
 def homePage():
     return render_template("index.html")
 
+@app.route('/form')
+def form():
+    return redirect(url_for('form'))
+
 @app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
 @cross_origin()
 def index():
-    
             #  reading the inputs given by the user
             gre_score=int(request.form['gre_score'])
             toefl_score = float(request.form['toefl_score'])
@@ -38,8 +93,7 @@ def index():
             return render_template('index.html', prediction_text='Admission chance is {}%'.format(output))
             #return render_template('index.html',prediction_text=round(100*prediction[0]))
         
-
-
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001)
-	app.run(debug=True)
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+    app.run(debug = True)
