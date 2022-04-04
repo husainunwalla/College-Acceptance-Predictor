0793f07ea9fac2ec02bf429ccaf515b2c165aa69
husainunwalla <mr.husainunwalla@gmail.com>
2022-04-04 20:31:13 +0530

    personality page and html streamlining

--- Application/main.py
+++ Application/main.py
@@ -1,6 +1,3 @@
-# importing the necessary dependencies
-# Husain - Added new import for addional functionality
-#from flask import Flask, render_template, request,jsonify
 from flask import (
     Flask,
     g,
@@ -12,11 +9,8 @@ from flask import (
     jsonify
 )
 from flask_cors import CORS, cross_origin
-import pickle
 import predict
-import json
 # Husain - class to store user type data
-
 class University:
     def __init__(self, name, val):
         self.name = name
@@ -29,6 +23,15 @@ class User:
 
     def __repr__(self):
         return f'<User: {self.username}>'
+
+class PersonalityQuestions():
+    def __init__(self, id, label):
+        self.id = id
+        self.label = label
+
+#Husain create questions
+ques = []
+ques.append(PersonalityQuestions(id='ques_1', label='Question 1'))
 
 # Husain = create usersnames and passwords that will work during login
 users = []
@@ -63,11 +66,26 @@ def login():
         user = [x for x in users if x.username == username][0]
         if user and user.password == password:
             session['user_id'] = user.id
-            return redirect(url_for('form'))
+            return redirect(url_for('home'))
 
         return redirect(url_for('login'))
 
     return render_template('login.html')
+
+#Page route to Home
+@app.route('/home', methods = ['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        if request.form['Form'] == 'Go to Form':
+            return redirect(url_for('form'))
+        if request.form['Form'] == 'Go to Personality test':
+            return redirect(url_for('personality'))
+    return render_template("home.html")
+
+#Husain : Page Route to Personality
+@app.route('/personality' , methods = ['GET', 'POST'])
+def personality():
+    return render_template("personality.html" , ques = ques)
 
 # Husain : Page route to form page
 @app.route('/form', methods=['GET', 'POST'])
@@ -81,34 +99,6 @@ def homePage():
     # Husain - changed default route to login
     # return render_template("index.html")
     return redirect(url_for('login'))
-
-#Jainam : route to show the predictions in a web UI
-'''@app.route('/predict', methods=['POST', 'GET'])
-@cross_origin()
-def index():
-    #  reading the inputs given by the user
-    gre_score = int(request.form['gre_score'])
-    toefl_score = float(request.form['toefl_score'])
-    university_rating = int(request.form['university_rating'])
-    sop = float(request.form['sop'])
-    lor = float(request.form['lor'])
-    cgpa = float(request.form['cgpa'])
-    is_research = request.form['research']
-    if(is_research == 'yes'):
-        research = 1
-    else:
-        research = 0
-    filename = 'finalized_model.pickle'
-    # loading the model file from the storage
-    loaded_model = pickle.load(open(filename, 'rb'))
-    # predictions using the loaded model file
-    prediction = loaded_model.predict(
-        [[gre_score, toefl_score, university_rating, sop, lor, cgpa, research]])
-    #print('predicted value is', prediction)
-    # showing the prediction results in a UI
-    output = round(prediction[0], 2)*100
-    return render_template('form.html', prediction_text='Admission chance is {}%'.format(output))
-    # return render_template('index.html',prediction_text=round(100*prediction[0]))'''
 
 #Husain : new predict method
 @app.route('/predictAll', methods=['POST', 'GET'])
@@ -130,42 +120,6 @@ def predict_all():
         unis.append(University(x['name'], x['val']))
     print(type(unis))
     return render_template("table.html", unis = unis)
-    '''
-    #  reading the inputs given by the user
-    gre_score = int(request.form['gre_score'])
-    toefl_score = float(request.form['toefl_score'])
-    university_rating = int(request.form['university_rating'])
-    sop = float(request.form['sop'])
-    lor = float(request.form['lor'])
-    cgpa = float(request.form['cgpa'])
-    is_research = request.form['research']
-    if(is_research == 'yes'):
-        research = 1
-    else:
-        research = 0
-    filename = 'finalized_model.pickle'
-    # loading the model file from the storage
-    loaded_model = pickle.load(open(filename, 'rb'))
-    # predictions using the loaded model file
-    prediction = loaded_model.predict(
-        [[gre_score, toefl_score, university_rating, sop, lor, cgpa, research]])
-    #print('predicted value is', prediction)
-    # showing the prediction results in a UI
-    output = round(prediction[0], 2)*100
-    return render_template('form.html', prediction_text='Admission chance is {}%'.format(output))'''
-    # return render_template('index.html',prediction_text=round(100*prediction[0]))
-
-#Husain : dummy method to print message in javascript
-@app.route('/test', methods=['GET', 'POST'])
-def testfn():
-    # GET request
-    if request.method == 'GET':
-        message = {'greeting':'Hello from Flask!'}
-        return jsonify(message)  # serialize and use JSON headers
-    # POST request
-    if request.method == 'POST':
-        print(request.get_json())  # parse as JSON
-        return 'Sucesss', 200
 
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001)
