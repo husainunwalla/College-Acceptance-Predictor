b5d091132282d80205a0e51d276e3ee986303bbc
Husain Unwalla <36625317+husainunwalla@users.noreply.github.com>
2022-04-08 18:28:50 +0530

    Code structuring  (#6)
    * personality page and html streamlining
    
    * code structuring
    
    * code structuring

--- Application/main.py
+++ Application/main.py
@@ -1,6 +1,3 @@
-# importing the necessary dependencies
-# Husain - Added new import for addional functionality
-#from flask import Flask, render_template, request,jsonify
 from flask import (
     Flask,
     g,
@@ -12,29 +9,11 @@ from flask import (
     jsonify
 )
 from flask_cors import CORS, cross_origin
-import pickle
 import predict
-import json
-# Husain - class to store user type data
+from dynamic.utility import University, User, PersonalityQuestions
 
-class University:
-    def __init__(self, name, val):
-        self.name = name
-        self.val = val
-class User:
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
-
-    def __repr__(self):
-        return f'<User: {self.username}>'
-
-# Husain = create usersnames and passwords that will work during login
-users = []
-users.append(User(id=1, username='Husain', password='password'))
-users.append(User(id=2, username='Jainam', password='password'))
-users.append(User(id=3, username='Isha', password='password'))
+questions = PersonalityQuestions.get_questions();
+users = User.get_users();
 
 # Jainam : initializing a flask app
 app = Flask(__name__)
@@ -56,18 +35,29 @@ def before_request():
 def login():
     if request.method == 'POST':
         session.pop('user_id', None)
-
         username = request.form['username']
         password = request.form['password']
-
         user = [x for x in users if x.username == username][0]
         if user and user.password == password:
             session['user_id'] = user.id
-            return redirect(url_for('form'))
-
+            return redirect(url_for('home'))
         return redirect(url_for('login'))
-
     return render_template('login.html')
+
+#Page route to Home
+@app.route('/home', methods = ['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        if request.form['Form'] == 'Go to Form':
+            return redirect(url_for('form'))
+        if request.form['Form'] == 'Go to Personality test':
+            return redirect(url_for('personality'))
+    return render_template("home.html")
+
+#Husain : Page Route to Personality
+@app.route('/personality' , methods = ['GET', 'POST'])
+def personality():
+    return render_template("personality.html" , ques = questions)
 
 # Husain : Page route to form page
 @app.route('/form', methods=['GET', 'POST'])
@@ -81,34 +71,6 @@ def homePage():
     # Husain - changed default route to login
     # return render_template("index.html")
     return redirect(url_for('login'))
-
-#Jainam : route to show the predictions in a web UI
-'''@app.route('/predict', methods=['POST', 'GET'])
-@cross_origin()
-def index():
-    #  reading the inputs given by the user
-    gre_score = int(request.form['gre_score'])
-    toefl_score = float(request.form['toefl_score'])
-    university_rating = int(request.form['university_rating'])
-    sop = float(request.form['sop'])
-    lor = float(request.form['lor'])
-    cgpa = float(request.form['cgpa'])
-    is_research = request.form['research']
-    if(is_research == 'yes'):
-        research = 1
-    else:
-        research = 0
-    filename = 'finalized_model.pickle'
-    # loading the model file from the storage
-    loaded_model = pickle.load(open(filename, 'rb'))
-    # predictions using the loaded model file
-    prediction = loaded_model.predict(
-        [[gre_score, toefl_score, university_rating, sop, lor, cgpa, research]])
-    #print('predicted value is', prediction)
-    # showing the prediction results in a UI
-    output = round(prediction[0], 2)*100
-    return render_template('form.html', prediction_text='Admission chance is {}%'.format(output))
-    # return render_template('index.html',prediction_text=round(100*prediction[0]))'''
 
 #Husain : new predict method
 @app.route('/predictAll', methods=['POST', 'GET'])
@@ -130,42 +92,6 @@ def predict_all():
         unis.append(University(x['name'], x['val']))
     print(type(unis))
     return render_template("table.html", unis = unis)
-    '''
-    #  reading the inputs given by the user
-    gre_score = int(request.form['gre_score'])
-    toefl_score = float(request.form['toefl_score'])
-    university_rating = int(request.form['university_rating'])
-    sop = float(request.form['sop'])
-    lor = float(request.form['lor'])
-    cgpa = float(request.form['cgpa'])
-    is_research = request.form['research']
-    if(is_research == 'yes'):
-        research = 1
-    else:
-        research = 0
-    filename = 'finalized_model.pickle'
-    # loading the model file from the storage
-    loaded_model = pickle.load(open(filename, 'rb'))
-    # predictions using the loaded model file
-    prediction = loaded_model.predict(
-        [[gre_score, toefl_score, university_rating, sop, lor, cgpa, research]])
-    #print('predicted value is', prediction)
-    # showing the prediction results in a UI
-    output = round(prediction[0], 2)*100
-    return render_template('form.html', prediction_text='Admission chance is {}%'.format(output))'''
-    # return render_template('index.html',prediction_text=round(100*prediction[0]))
-
-#Husain : dummy method to print message in javascript
-@app.route('/test', methods=['GET', 'POST'])
-def testfn():
-    # GET request
-    if request.method == 'GET':
-        message = {'greeting':'Hello from Flask!'}
-        return jsonify(message)  # serialize and use JSON headers
-    # POST request
-    if request.method == 'POST':
-        print(request.get_json())  # parse as JSON
-        return 'Sucesss', 200
 
 if __name__ == "__main__":
     #app.run(host='127.0.0.1', port=8001)
--- Application/static/js/script.js
+++ Application/static/js/script.js
@@ -1,10 +1,3 @@
-function predict() {
-    fetch('/predictAll')
-        .then(function (response) {
-            return response.json();
-        }).then(function (text) {
-            console.log('GET response:');
-            my_dat = JSON.parse(text)
-            console.log(my_dat);
-        });
+function showAlert(msg) {
+    alert(msg + " Coming soon!");
 }
